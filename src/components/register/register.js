9b79e43324cb498d424d21382f9ce4089ce0d0dd
Guilherme Santos <guilherme.a.sts@gmail.com>
2024-04-18 17:09:34 -0300

    Uptade 4 - Atulização no css do register

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -3,7 +3,7 @@ import React from "react";
 function Register() {
     return (
       <div className='register'>
-        <p className='poppins-regular'>Não tem uma conta? Registre-se</p>
+        <p className='poppins-regular'>Não tem uma conta? </p> <p className="poppins-semibold">Registre-se</p>
       </div>
     );
   }
